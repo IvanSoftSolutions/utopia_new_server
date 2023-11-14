aedad766a5bd68c21b0bb845c010e55294fedf7d
ivand <ivan.daniel.hdz@gmail.com>
2023-11-14 15:22:01 -0600

    Render preparations

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import express from "express";
 import cors from 'cors';
 import mongoose from 'mongoose';
-import { PORT, mongodbURL } from '../utopia_new_server/config.js';
+require("dotenv").config();
+// import { PORT, mongodbURL } from '../utopia_new_server/config.js';
 import formulasRoute from './routes/formulaRoute.js';
 import bitacoraRoute from './routes/bitacoraRoute.js';
 import engraseRoute from './routes/engraseRoute.js';
@@ -22,21 +23,27 @@ app.use(express.json());
 
 //Middleware for handling CORS policy
 //Option 1: Allow all origins with default of cors(*)
-app.use(cors());
+// app.use(cors());
 
 //Option 2: Allow Custom Origins
-// app.use(
-//     cors({
-//         origin: ['http://localhost:3000'], // replace this to your frontend url
-//         methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//         allowedHeaders: ['Content-Type']
-//     })
-// );
+app.use(
+    cors({
+        origin: ['http://localhost:3000'], // replace this to your frontend url
+    })
+);
 
-// app.get('/', (req, res) => {
-//     console.log(req)
-//     return res.status(234).send('asdasdasd');
-// });
+mongoose
+    .connect(process.env.mongodbURL)
+    .then(() => {
+        console.log("App connected to database");
+        const PORT = process.env.PORT || 8000
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.log("MongoDB connection error: ", error);
+    })
 
 //FORMULAS ROUTES
 app.use('/', formulasRoute);
@@ -78,15 +85,3 @@ app.use('/', maquilasVentasRoute);
 
 //MAQUILAS_CONCEPTOS ROUTES
 app.use('/', maquilasConceptosRoute)
-
-mongoose
-    .connect(mongodbURL)
-    .then(() => {
-        console.log("App connected to database");
-        app.listen(PORT, () => {
-            console.log(`Server is running on port ${PORT}`);
-        });
-    })
-    .catch((error) => {
-        console.log("MongoDB connection error: ", error);
-    })
